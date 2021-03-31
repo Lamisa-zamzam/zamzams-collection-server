d5c7a235ad9174f3f8195887d0d9a407c8730a1a
Your Name <lamisazamzam@gmail.com>
2021-04-01 00:38:49 +0600

    First Commmit

--- index.js
+++ index.js
@@ -2,6 +2,52 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+const ObjectID = require("mongodb").ObjectID;
+
+require("dotenv").config();
+
+const MongoClient = require("mongodb").MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hwuiv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(
+    uri,
+    { useUnifiedTopology: true },
+    { useNewUrlParser: true }
+);
+
+client.connect((err) => {
+    const productCollection = client
+        .db(process.env.DB_NAME)
+        .collection("products");
+    console.log("db connected");
+
+    app.post("/addProduct", (req, res) => {
+        const product = req.body;
+
+        productCollection
+            .insertMany(product)
+            .then((result) => res.send(result.insertedCount > 0));
+    });
+
+    app.get("/products", (req, res) => {
+        productCollection.find({})
+        .toArray((err, documents) => {
+            res.send(documents);
+        })
+    })
+
+    app.get("/product/:_id", (req, res) => {
+        const _id = ObjectID(req.params._id);
+        productCollection.find({_id: _id})
+        .toArray((err, documents) => {
+            res.send(documents[0]);
+        })
+    })
+});
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
